28078b050bc5a89aa956fc4fb84f162dd04b2b6b
Madathanapalleleena <leena91919@gmail.com>
2025-09-13 20:29:05 +0530

    Added intial simulation files

---
+++ train-sim/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TrainSimulation from "./components/TrainSimulation";
+
+function App() {
+  return (
+    <div>
+      <TrainSimulation />
+    </div>
+  );
+}
+
+export default App;
